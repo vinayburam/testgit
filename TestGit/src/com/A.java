0e5f81f5d68fea681f579e5717e82305e74593bb
VINAY <VINAY@WIN-RP4BLDH6PHQ>
2018-05-06 12:16:34 +0530

    new method added

--- TestGit/src/com/A.java
+++ TestGit/src/com/A.java
@@ -1,6 +1,11 @@
 package com;
 
 public class A {
+	
+	public void m2() {
+		
+		System.out.println("M2 method of class A");
+	}
 	public static void main(String[] args) {
 		System.out.println("In Main");
 	}
